95bb16a34ae07cc98ab0d36619d9dd99e04216bb
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-11-26 21:35:46 +0100

    bumped to version 0.2.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), 'r') as f:
     long_description = f.read()
 
-version = "0.2.4"
+version = "0.2.5"
 
 setup(
     name='aws-ssm-copy',
