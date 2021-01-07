f2b51aa6460c45ee9618eacd9a3e8ab893ee31e2
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2021-01-07 16:25:32 +0100

    bumped to version 0.3.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), 'r') as f:
     long_description = f.read()
 
-version = "0.3.2"
+version = "0.3.3"
 
 setup(
     name='aws-ssm-copy',
